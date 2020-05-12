d7d46a7f554563e58cd13057761d25c5272aba9f
jean-brum <jeanpaulodebrum@gmail.com>
2020-05-12 18:49:49 -0300

    views

--- iniciando-com-laravel-55/my-app-laravel/readme.md
+++ iniciando-com-laravel-55/my-app-laravel/readme.md
@@ -1,3 +1,9 @@
-PARA INICIAR O SERVIDOR LOCAL:
+PARA INICIAR O SERVIDOR LOCAL: `php artisan serve`
 
-	php artisan serve
+-----------------------------------------------------------------------------------------------
+
+PARA CRIAR NOVAS ROTAS, DEVE-SE ACESSAR O DIRETÓRIO `routes/web.php` E CRIAR AS ROTAS DESEJADAS.
+PARA CRIAR NOVAS VIEWS, DEVE-SE ACESSAR O DIRETÓRIO `resources/views` E CRIAR AS VIEWS DESEJADAS.
+
+-----------------------------------------------------------------------------------------------
+
--- iniciando-com-laravel-55/my-app-laravel/routes/web.php
+++ iniciando-com-laravel-55/my-app-laravel/routes/web.php
@@ -11,6 +11,70 @@
 |
 */
 
+// MVC - Model - View - Controller
+
+// O helper view() recebe o nome de um arquivo e retorna o seu html - por padrão está em resources/views
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // helper - em laravel, é uma função
 });
+
+Route::get('/cliente/cadastrar', function () {
+    $nome = 'Jean';
+    return view('cliente.cadastrar', compact('nome'));
+        // [
+        //'nome' => $nome
+        //];
+    // O helper view recebe um segundo parâmetro com todos os valores que podem ser usadas pelo view.
+    // E para usar na view, apenas usar a variável com o nome do valor.
+});
+
+//Route::get('/cliente', function () {
+//    // CSRF - Token
+//    // Temos que enviar também, o Token
+//    $csrfToken = csrf_token();
+//
+//    // É necessário criar um campo hidden com o name: _token e o value: csrf_token();
+//    $html = <<<HTML
+//<html>
+//    <body>
+//    <h1>Cliente</h1>
+//        <form action="/cliente/cadastrar" method="post">
+//            <input type="hidden" name="_token" value="$csrfToken" >
+//            <input type="text" name="name" id="name">
+//            <button type="submit">Enviar</button>
+//        </form>
+//    </body>
+//</html>
+//HTML;
+//    return $html;
+//});
+
+// Deve-se passar um $request do tipo Request como parâmetro para pegar as requisições.
+// Após isto, é necessário usar o método get() com o name da requisição.
+//Route::post('/cliente/cadastrar', function (\Illuminate\Http\Request $request) {
+//   echo "Cliente " . $request->get('name'); // Com este get(), é possível pegar tanto POST como GET.
+//    // Ou
+//   echo "Cliente " . $request->name;
+//});
+
+//Route::get('/admin/cliente', function () {
+//    return "Admin - Hello World!";
+//});
+//
+//Route::get('/cliente-echo', function () {
+//    echo "Texto com echo";
+//});
+//
+//// Passando parâmetros para a rota
+//// Neste exemplo, o nome do produto deve ser informado na rota
+//// Deve-se passar o parâmetro da rota como parâmetro na função também
+//Route::get('/produto/{name}/{id}', function ($name, $id) {
+//    return "Produto: $name - Id: $id";
+//});
+//
+//// Passando parâmetros opcionais para a rota
+//// Deve-se colocar um "?" após o parâmetro da rota, como no caso do "{id?}"
+//// Depois disso também deve-se passar um valor padrão para o parâmetro na function. Ex: $id = "Sem Id"
+//Route::get('/fornecedor/{name}/{id?}', function ($name, $id = "Sem Id") {
+//        return "Produto: $name - Id: $id";
+//});
